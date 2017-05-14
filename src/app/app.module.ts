1ef5c60dc66d7582ab3302548d7c66d0181d6286
Łukasz Gałka <lukasz.galka@intive.com>
2017-05-14 19:41:18 +0200

    split app component into demo and custom demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,14 @@ import { NgxLoremIpsumModule } from 'ngx-lorem-ipsum';
 import { AppComponent } from './app.component';
 import { OptionsViewerComponent } from './options-viewer.component';
 
+import { DemoComponent } from './demo/demo.component';
+import { CustomDemoComponent } from './custom-demo/custom-demo.component';
+
 @NgModule({
     declarations: [
         AppComponent,
+        DemoComponent,
+        CustomDemoComponent,
         OptionsViewerComponent
     ],
     imports: [
@@ -23,7 +28,10 @@ import { OptionsViewerComponent } from './options-viewer.component';
         HttpModule,
         NgxGalleryModule,
         NgxLoremIpsumModule,
-        RouterModule.forRoot([]),
+        RouterModule.forRoot([
+            { path: '', component: DemoComponent },
+            { path: 'custom', component: CustomDemoComponent }
+        ]),
         Ng2PageScrollModule.forRoot()
     ],
     providers: [],
